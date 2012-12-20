ded6855e596fb1718aa17d2f225c082836f7e924
Anshuman Bhaduri <anshuman.bhaduri0@gmail.com>
2012-12-20 23:41:22 +1300

    Initial commit.
    --HG--
    extra : amend_source : 6331cd811b02b260e0aa0459c7e67bca784c1141

---
+++ setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1'
+
+long_description = (
+    read('README.rst')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('AUTHORS')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.rst')
+    + '\n' +
+   'Download\n'
+    '********\n')
+
+entry_point = 'bowerrecipe:Recipe'
+entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+
+tests_require = ['zope.testing', 'zc.buildout']
+
+setup(name='bowerrecipe',
+      version=version,
+      description="zc.buildout recipe to install static resources using Twitter Bower.",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        ],
+      keywords='',
+      author='Anshuman Bhaduri',
+      author_email='anshuman.bhaduri0@gmail.com',
+      url='http://github.com/anshumanb/bowerrecipe',
+      license='mit',
+      packages=find_packages(exclude=['ez_setup']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite='bowerrecipe.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
